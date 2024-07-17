079d5635f156fd0e041ea4e0fa4abc8ca2ddc91d
김의현(toma_to) <148212609+tomaho1756@users.noreply.github.com>
2024-07-17 22:24:37 +0900

    Update store.ts

--- src/state/store.ts
+++ src/state/store.ts
@@ -5,4 +5,7 @@ export const store = createStore(() => ({
         accessToken: "",
         refreshToken: "",
     },
+    lang: {
+        langList: ["작은 실천으로 시작하는","세계시민","로그인", "회원가입", "로그인 성공", "로그인 실패", "게시물", "캘린더", "메인", "이슈", "유저", "챌린지 하러가기", "눌러서 자세히볼 수 있어요!"]
+    }
 }));
